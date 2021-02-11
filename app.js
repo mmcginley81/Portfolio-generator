40c17cc8777b876f9ff1e48faf00a1e8e501c4fe
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-02-10 18:01:04 -0800

    added inquirer package

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
+const inquirer = require('inquirer');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(names, github)
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(names, github), err => {
+//   if (err) throw new Error(err);
 
-const generatePage = (names, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  <body>
-    <h1>${names}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+// console.log(names, github)
+// console.log(generatePage(names, github));
 
-fs.writeFile('./index.html', generatePage(names, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-console.log(names, github)
-console.log(generatePage(names, github));
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
